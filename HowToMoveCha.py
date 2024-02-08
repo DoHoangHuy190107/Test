ba338ba0f3b65415d85ba09dc62888fe3e2d4342
DoHoangHuy190107 <158984070+DoHoangHuy190107@users.noreply.github.com>
2024-02-08 13:56:45 +0500

    Update HowToMoveCha.py
    Left-Right-Up-Down

--- HowToMoveCha.py
+++ HowToMoveCha.py
@@ -10,49 +10,55 @@ black = (0, 0, 0)
 
 screen = pygame.display.set_mode((width, height))
 
-
-def draw(player):
-    screen.fill(black)
-    pygame.draw.rect(screen, white, player)
-    pygame.display.update()
+class Player(pygame.sprite.Sprite):
+    def __init__(self, col, x, y):
+        super().__init__()
+        self.rect = pygame.Rect(x, y, 30, 50)
+        self.X = x
+        self.Y = y
+        self.col = col
+    def draw(self):
+        pygame.draw.rect(screen, self.col, (self.X, self.Y, 30, 50))
+        pygame.display.update()
+    def handle_event(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            if keys[pygame.K_DOWN]:
+                self.X -= VelPlayer * 7 / 10
+                self.Y += VelPlayer * 7 / 10
+            elif keys[pygame.K_UP]:
+                self.X -= VelPlayer * 7/ 10
+                self.Y -= VelPlayer * 7 / 10
+            else:
+                self.X -= VelPlayer
+        elif keys[pygame.K_RIGHT]:
+            if keys[pygame.K_DOWN]:
+                self.X += VelPlayer * 7 / 10
+                self.Y += VelPlayer * 7 / 10
+            elif keys[pygame.K_UP]:
+                self.X += VelPlayer * 7 / 10
+                self.Y -= VelPlayer * 7 / 10
+            else :
+                self.X += VelPlayer
+        elif keys[pygame.K_DOWN]:
+            self.Y += VelPlayer
+        elif keys[pygame.K_UP]:
+            self.Y -= VelPlayer
 
 def main():
     run = True
-    player = pygame.Rect((width - X) / 2, (height - Y) / 2, X, Y)
+    player = Player(white, 300, 300)
     clock = pygame.time.Clock()
 
     while(run):
-        clock.tick(100)  
-
+        clock.tick(40)  
+        screen.fill(black)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            if keys[pygame.K_DOWN]:
-                player.x -= VelPlayer / 2
-                player.y += VelPlayer / 2
-            elif keys[pygame.K_UP]:
-                player.x -= VelPlayer / 2
-                player.y -= VelPlayer / 2
-            else:
-                player.x -= VelPlayer
-        elif keys[pygame.K_RIGHT]:
-            if keys[pygame.K_DOWN]:
-                player.x += VelPlayer / 2
-                player.y += VelPlayer / 2
-            elif keys[pygame.K_UP]:
-                player.x += VelPlayer / 2
-                player.y -= VelPlayer / 2
-            else :
-                player.x += VelPlayer
-        elif keys[pygame.K_DOWN]:
-            player.y += VelPlayer
-        elif keys[pygame.K_UP]:
-            player.y -= VelPlayer
-        draw(player)
+        player.handle_event()
+        player.draw()
 
 if __name__ == "__main__":
     main()
