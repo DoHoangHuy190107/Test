56bd72a33c27e987ed4daca236c0825d1ff0b8cd
DoHoangHuy190107 <158984070+DoHoangHuy190107@users.noreply.github.com>
2024-02-18 17:53:09 +0500

    Update Button.py

--- Button.py
+++ Button.py
@@ -5,10 +5,6 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 font = pygame.font.SysFont(None, 30)
 
-#Create images of button
-play_button = pygame.image.load("PlayButton.png").convert_alpha()
-guide_button = pygame.image.load("GuideButton.png").convert_alpha()
-quit_button = pygame.image.load("New Piskel (10).png").convert_alpha()
 #button class
 class Button():
 	def __init__(self, x, y, image, scale):
@@ -37,37 +33,3 @@ class Button():
 		screen.blit(self.image, (self.rect.x, self.rect.y))
 
 		return action
-
-
-
-def GUIDE():
-      draw_text("This is Guide", font, 200, 200, (50, 20, 100))
-
-def QUIT():
-    pygame.quit()
-    sys.exit()
-
-	
-def main():
-    play = Button(340, 200, play_button, 2)
-    guide = Button(340, 250, guide_button, 2)
-    quit = Button(340, 300, quit_button, 2)
-    t = 1
-    while True: 
-        if play.draw():
-            print(t)
-            t += 1
-        if guide.draw():
-            GUIDE()
-        if quit.draw():
-            QUIT()
-		
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        pygame.display.flip()
-
-
-if __name__ == "__main__":
-	main()
